2a04103e7750fd512402f8825760637b68de8de7
John Joseph II <jackjoseph7447@gmail.com>
2024-11-07 11:42:17 -0500

    Intent used

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,9 +1,13 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+
+private const val KEY_STRING = "words"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +19,12 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            startActivity(
+                Intent(Intent.ACTION_SEND).apply {
+                    putExtra(KEY_STRING, editText.text)
+                    type = "text/plain"
+                }
+            )
         }
     }
 }
